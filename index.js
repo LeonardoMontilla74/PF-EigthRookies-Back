cabe38346838521955672ddf3e2da2c7d65285db
Leonardo Montilla <leme.310589@gmail.com>
2022-06-30 02:04:26 -0400

    Deployd post demo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const{CargarTodo}=require('./src/controladores/cargaProductosBDv1')
 const { cargarUsuario, arrObj } = require('./src/controladores/cargarAdmin')
 const PORT = process.env.PORT || 3001
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server at on port ${PORT}`); // eslint-disable-line no-console
     CargarTodo()
